cbe39969b5c7ca5da6b729a883aed6937b5f180d
Theodore Negusu <44190023+baydisng13@users.noreply.github.com>
2022-11-14 12:08:22 +0300

    js starter

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center h-screen bg-gray-900 text-white">
+     
+      <h1 className='text-4xl font-bold mb-4 flex gap-2'>
+        <span className="text-[#f7c929]">Vite</span> + 
+        <span className="text-[#61DBFB]"> React </span>  + 
+        <span className="text-[#38bdf8]">Tailwind</span> +
+        <span className="text-[#f44250]"> React Router</span>   : 
+        <span className='px-4 py-2 bg-[#efd81d] text-black rounded-full'>JS Starter</span>
+      </h1>
+      <p className="text-white">let me know if there is anything you would like to add to</p>
+      <p className="text-white"> Created By <a className="text-indigo-600" href="https://github.com/baydisng13">Theodore</a></p>
+
+    </div>
+  )
+}
+
+export default App
